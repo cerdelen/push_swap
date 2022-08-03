8d3c7606ff7668f75f4b0d99e49f8f68c839fd94
cerdelen <cerdelen@cerdelens-MacBook-Air.local>
2022-08-03 18:16:16 +0200

    quicker radix sort by approx 10%

--- main.c
+++ main.c
@@ -12,28 +12,6 @@
 
 #include "push_swap.h"
 
-void	print_2d_array(char	**arr, int fd)
-{
-	int	i;
-
-	i = 0;
-	if (arr)
-	{
-		while (arr[i] != NULL)
-		{
-			ft_putstr_fd(arr[i], fd);
-			if (arr[i][ft_strlen(arr[i]) - 1] != '\n')
-				ft_putchar_fd('\n', fd);
-			i++;
-		}
-	}
-}
-
-void	print_lst_content_int(void *content)
-{
-	printf("%i\n", 	*((int *)content));
-}
-
 int	main(int argc, char **argv)
 {
 	t_list	*stack_a;
@@ -55,12 +33,5 @@ int	main(int argc, char **argv)
 		sort_small(&stack_a, &stack_b, argc);
 	else
 		sortbigstack(&stack_a, &stack_b);
-	
-	// printf("\nlist a \n\n");
-	// ft_lstiter(stack_a, print_lst_content_int);
-	// printf("\nlist b \n\n");
-	// ft_lstiter(stack_b, print_lst_content_int);
-
-
 	return (free_remains(&stack_a, &stack_b, tab));
 }
